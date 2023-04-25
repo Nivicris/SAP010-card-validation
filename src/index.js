fe6d506b33b22346fe66c250651f3816a617d359
Nívia Miranda <niviacristinamiranda@gmail.com>
2023-04-25 00:03:02 -0300

    primeiros codigos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import validator from './validator.js';
 
-console.log(validator);
+// function uppercase() {
+//     let texto = document.getElementById("nome").value;
+//     let textoMaiusculo = texto.toUpperCase();
+//     alert(textoMaiusculo);
+//   }
+
+// const
+// validator.isValid(creditCardNumber): creditCardNumber
+// validator.maskify(creditCardNumber): creditCardNumber
+
+const cartaoCredito = document.getElementById ("numero-cartao");
+
+
+
+// document.getElementById("validar").addEventListener("click", meubotao())
+
+// function meubotao(){
+//    var cartao = document.getElementById("numero-cartao")
+//    console.log(cartao)
+//    document.getElementById("result").innerHTML = "O numero do seu cartão é" + cartao + "!"
+// }
+
+
+
+{
+console.log(validator);}
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,26 @@
 const validator = {
-  // ...
+ 
+maskify: function (numeroCartao) {
+  const caracterExibidos = numeroCartao.slice(-4);
+  const caracterMascarado = numeroCartao.slice (0, -4).replace(/./g, "*");
+  const cartaoCreditoMascarado = caracterMascara + caracterExibidos;
+
+return cartaoCreditoMascarado
+
 };
+// utilizei o slice para mostrar os 4 ultimos numeros do cartão a serem exibidos
+// depois tmb utilizei o slice para extrair a parte da string a ser substituida por * (replace)
+// concatenei dentro de uma contante o numero mascarado+exibido
+
+isValid: function (numeroCartao)
+
+
+
+
+
+
+
+};
+
 
 export default validator;
